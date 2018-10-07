f97f6102810dbd652aca9dbbfcde9705615f3d54
Mitchell Monaghan <mitchell.monaghan@hotmail.com>
2018-10-07 00:43:31 -0500

    Implemented cron job for cleaning up notifications older than a week.

--- server/src/graphql/index.js
+++ server/src/graphql/index.js
@@ -5,6 +5,7 @@ import { ApolloServer, makeExecutableSchema } from 'apollo-server'
 import { Prisma, extractFragmentReplacements } from 'prisma-binding'
 import { get, merge } from 'lodash'
 import jwt from 'jsonwebtoken'
+import { CronJob } from 'cron'
 
 import { getUserFromToken } from '@services/jwt'
 import { verifyEmail } from '@modules/auth/manager'
@@ -34,8 +35,7 @@ const graphqlServer = new ApolloServer({
       fragmentReplacements,
       typeDefs: 'src/graphql/generated/prisma.graphql',
       endpoint: config.prismaUrl,
-      secret: config.prismaSecret,
-      debug: config.env !== 'production'
+      secret: config.prismaSecret
     })
 
     if (context.authToken) {
@@ -67,6 +67,28 @@ const graphqlServer = new ApolloServer({
     apiKey: config.apolloEngineAPIKey
   }
 })
+
+// Cron jobs
+// depending on how this is deployed this might not be needed and should be
+// executed by something like https://elements.heroku.com/addons/scheduler
+const cleanUpNotificationsJob = new CronJob('0 0 0 * * *', async () => {
+  const prisma = new Prisma({
+    typeDefs: 'src/graphql/generated/prisma.graphql',
+    endpoint: config.prismaUrl,
+    secret: config.prismaSecret
+  })
+
+  const oneWeekAgo = new Date()
+  oneWeekAgo.setDate(oneWeekAgo.getDate() - 7)
+
+  const notifications = await prisma.query.notifications({ where: { createdAt_lt: oneWeekAgo } })
+
+  notifications.forEach(notification => {
+    prisma.mutation.deleteNotification({ where: { id: notification.id } })
+  })
+})
+
+cleanUpNotificationsJob.start()
 
 module.exports = graphqlServer
 export default graphqlServer
